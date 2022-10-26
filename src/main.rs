e4326a200aba74db519cd7e3d901f00f66ef70e9
Jonas <jonas@jmp.blue>
2022-10-26 22:57:31 +0200

    + add basic movement

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use bevy::ecs::query::QuerySingleError;
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
+use bevy::utils::{HashMap, HashSet};
 use bevy::{asset::AssetServerSettings, render::texture::ImageSettings};
 use bevy_ecs_ldtk::prelude::*;
 use bevy_inspector_egui::WorldInspectorPlugin;
+use bevy_rapier2d::prelude::*;
 use leafwing_input_manager::prelude::*;
 
 fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(Camera2dBundle::default());
+    commands.spawn_bundle(Camera2dBundle {
+        ..Default::default()
+    });
+    // commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
     commands.spawn_bundle(LdtkWorldBundle {
         ldtk_handle: asset_server.load("world.ldtk"),
@@ -16,9 +21,10 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
 fn main() {
     App::new()
+        .insert_resource(ImageSettings::default_nearest())
         .insert_resource(WindowDescriptor {
-            width: 1270.0,
-            height: 720.0,
+            width: 1920.0,
+            height: 1080.0,
             title: String::from("Mogus"),
             ..Default::default()
         })
@@ -26,15 +32,30 @@ fn main() {
             watch_for_changes: true,
             ..default()
         })
-        .insert_resource(ImageSettings::default_nearest())
-        .insert_resource(LevelSelection::Index(0))
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        // .add_plugin(RapierDebugRenderPlugin::default())
+        .insert_resource(LdtkSettings {
+            level_spawn_behavior: LevelSpawnBehavior::UseWorldTranslation {
+                load_level_neighbors: true,
+            },
+            set_clear_color: SetClearColor::No,
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
         .add_plugin(LdtkPlugin)
+        .insert_resource(LevelSelection::Uid(0))
         .register_ldtk_entity::<PlayerBundle>("Player")
+        .register_ldtk_int_cell::<WallBundle>(1)
+        .register_ldtk_int_cell::<WallBundle>(4)
         .add_plugin(InputManagerPlugin::<Action>::default())
         .add_startup_system(startup)
+        .add_system(spawn_wall_collision)
         .add_system(init_player)
         .add_system(jump)
+        .add_system(move_player)
+        .add_system(camera_fit_inside_current_level)
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_plugin(WorldInspectorPlugin::new())
         .run();
 }
@@ -47,8 +68,8 @@ pub struct PlayerBundle {
     #[bundle]
     #[sprite_sheet_bundle(
         "sprites/tilemaps/characters_packed.png",
-        18.0,
-        18.0,
+        24.0,
+        24.0,
         9,
         3,
         0.0,
@@ -58,13 +79,12 @@ pub struct PlayerBundle {
     sprite_sheet: SpriteSheetBundle,
     #[worldly]
     worldly: Worldly,
-    #[grid_coords]
-    grid_coords: GridCoords,
     #[from_entity_instance]
     health: HealthValue,
     #[from_entity_instance]
     entity_instance: EntityInstance,
     player: Player,
+    velocity: Velocity,
 }
 
 #[derive(Default, Reflect, Component, Clone, Debug)]
@@ -92,10 +112,182 @@ impl From<EntityInstance> for HealthValue {
     }
 }
 
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Component)]
+pub struct Wall;
+
+#[derive(Clone, Debug, Default, Bundle, LdtkIntCell)]
+pub struct WallBundle {
+    wall: Wall,
+}
+
+pub fn spawn_wall_collision(
+    mut commands: Commands,
+    wall_query: Query<(&GridCoords, &Parent), Added<Wall>>,
+    parent_query: Query<&Parent, Without<Wall>>,
+    level_query: Query<(Entity, &Handle<LdtkLevel>)>,
+    levels: Res<Assets<LdtkLevel>>,
+) {
+    /// Represents a wide wall that is 1 tile tall
+    /// Used to spawn wall collisions
+    #[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Hash)]
+    struct Plate {
+        left: i32,
+        right: i32,
+    }
+
+    /// A simple rectangle type representing a wall of any size
+    #[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Hash)]
+    struct Rect {
+        left: i32,
+        right: i32,
+        top: i32,
+        bottom: i32,
+    }
+
+    // Consider where the walls are
+    // storing them as GridCoords in a HashSet for quick, easy lookup
+    //
+    // The key of this map will be the entity of the level the wall belongs to.
+    // This has two consequences in the resulting collision entities:
+    // 1. it forces the walls to be split along level boundaries
+    // 2. it lets us easily add the collision entities as children of the appropriate level entity
+    let mut level_to_wall_locations: HashMap<Entity, HashSet<GridCoords>> = HashMap::new();
+
+    wall_query.for_each(|(&grid_coords, parent)| {
+        // An intgrid tile's direct parent will be a layer entity, not the level entity
+        // To get the level entity, you need the tile's grandparent.
+        // This is where parent_query comes in.
+        if let Ok(grandparent) = parent_query.get(parent.get()) {
+            level_to_wall_locations
+                .entry(grandparent.get())
+                .or_insert(HashSet::new())
+                .insert(grid_coords);
+        }
+    });
+
+    if !wall_query.is_empty() {
+        level_query.for_each(|(level_entity, level_handle)| {
+            if let Some(level_walls) = level_to_wall_locations.get(&level_entity) {
+                let level = levels
+                    .get(level_handle)
+                    .expect("Level should be loaded by this point");
+
+                let LayerInstance {
+                    c_wid: width,
+                    c_hei: height,
+                    grid_size,
+                    ..
+                } = level
+                    .level
+                    .layer_instances
+                    .clone()
+                    .expect("Level asset should have layers")[0];
+
+                // combine wall tiles into flat "plates" in each individual row
+                let mut plate_stack: Vec<Vec<Plate>> = Vec::new();
+
+                for y in 0..height {
+                    let mut row_plates: Vec<Plate> = Vec::new();
+                    let mut plate_start = None;
+
+                    // + 1 to the width so the algorithm "terminates" plates that touch the right
+                    // edge
+                    for x in 0..width + 1 {
+                        match (plate_start, level_walls.contains(&GridCoords { x, y })) {
+                            (Some(s), false) => {
+                                row_plates.push(Plate {
+                                    left: s,
+                                    right: x - 1,
+                                });
+                                plate_start = None;
+                            }
+                            (None, true) => plate_start = Some(x),
+                            _ => (),
+                        }
+                    }
+
+                    plate_stack.push(row_plates);
+                }
+
+                // combine "plates" into rectangles across multiple rows
+                let mut wall_rects: Vec<Rect> = Vec::new();
+                let mut previous_rects: HashMap<Plate, Rect> = HashMap::new();
+
+                // an extra empty row so the algorithm "terminates" the rects that touch the top
+                // edge
+                plate_stack.push(Vec::new());
+
+                for (y, row) in plate_stack.iter().enumerate() {
+                    let mut current_rects: HashMap<Plate, Rect> = HashMap::new();
+                    for plate in row {
+                        if let Some(previous_rect) = previous_rects.remove(plate) {
+                            current_rects.insert(
+                                *plate,
+                                Rect {
+                                    top: previous_rect.top + 1,
+                                    ..previous_rect
+                                },
+                            );
+                        } else {
+                            current_rects.insert(
+                                *plate,
+                                Rect {
+                                    bottom: y as i32,
+                                    top: y as i32,
+                                    left: plate.left,
+                                    right: plate.right,
+                                },
+                            );
+                        }
+                    }
+
+                    // Any plates that weren't removed above have terminated
+                    wall_rects.append(&mut previous_rects.values().copied().collect());
+                    previous_rects = current_rects;
+                }
+
+                commands.entity(level_entity).with_children(|level| {
+                    // Spawn colliders for every rectangle..
+                    // Making the collider a child of the level serves two purposes:
+                    // 1. Adjusts the transforms to be relative to the level for free
+                    // 2. the colliders will be despawned automatically when levels unload
+                    for wall_rect in wall_rects {
+                        level
+                            .spawn()
+                            .insert(Collider::cuboid(
+                                (wall_rect.right as f32 - wall_rect.left as f32 + 1.)
+                                    * grid_size as f32
+                                    / 2.,
+                                (wall_rect.top as f32 - wall_rect.bottom as f32 + 1.)
+                                    * grid_size as f32
+                                    / 2.,
+                            ))
+                            .insert(RigidBody::Fixed)
+                            .insert(Friction {
+                                coefficient: 0.1,
+                                ..Default::default()
+                            })
+                            .insert(Transform::from_xyz(
+                                (wall_rect.left + wall_rect.right + 1) as f32 * grid_size as f32
+                                    / 2.,
+                                (wall_rect.bottom + wall_rect.top + 1) as f32 * grid_size as f32
+                                    / 2.,
+                                0.,
+                            ))
+                            .insert(GlobalTransform::default());
+                    }
+                });
+            }
+        });
+    }
+}
+
 #[derive(Actionlike, PartialEq, Eq, Clone, Copy, Hash, Debug)]
 enum Action {
     Run,
     Jump,
+    Left,
+    Right,
 }
 
 fn init_player(mut commands: Commands, query: Query<Entity, Added<Player>>) {
@@ -103,20 +295,108 @@ fn init_player(mut commands: Commands, query: Query<Entity, Added<Player>>) {
         commands
             .entity(entity)
             .insert(Player)
+            .insert(RigidBody::Dynamic)
+            .insert(Collider::cuboid(12.0, 12.0))
             .insert_bundle(InputManagerBundle::<Action> {
                 // Stores "which actions are currently pressed"
                 action_state: ActionState::default(),
                 // Describes how to convert from player inputs into those actions
-                input_map: InputMap::new([(KeyCode::Space, Action::Jump)]),
+                input_map: InputMap::new([
+                    // Jump
+                    (KeyCode::Space, Action::Jump),
+                    (KeyCode::Up, Action::Jump),
+                    (KeyCode::W, Action::Jump),
+                    // Left
+                    (KeyCode::Left, Action::Left),
+                    (KeyCode::A, Action::Left),
+                    // Right
+                    (KeyCode::Right, Action::Right),
+                    (KeyCode::D, Action::Right),
+                ]),
             });
     }
 }
 
-// Query for the `ActionState` component in your game logic systems!
-fn jump(query: Query<&ActionState<Action>, With<Player>>) {
-    if let Ok(action_state) = query.get_single() {
-        if action_state.just_pressed(Action::Jump) {
-            println!("I'm jumping!");
+fn jump(mut query: Query<(&ActionState<Action>, &mut Velocity), With<Player>>) {
+    if let Ok((action_state, mut velocity)) = query.get_single_mut() {
+        if action_state.pressed(Action::Jump) {
+            velocity.linvel = Vec2::new(velocity.linvel.x, 64.0);
+        }
+    }
+}
+
+fn move_player(mut query: Query<(&ActionState<Action>, &mut Velocity), With<Player>>) {
+    if let Ok((action_state, mut velocity)) = query.get_single_mut() {
+        if action_state.pressed(Action::Right) {
+            velocity.linvel = Vec2::new(32.0, velocity.linvel.y);
+        }
+
+        if action_state.pressed(Action::Left) {
+            velocity.linvel = Vec2::new(-32.0, velocity.linvel.y);
+        }
+    }
+}
+
+const ASPECT_RATIO: f32 = 16. / 9.;
+
+pub fn camera_fit_inside_current_level(
+    mut camera_query: Query<
+        (
+            &mut bevy::render::camera::OrthographicProjection,
+            &mut Transform,
+        ),
+        Without<Player>,
+    >,
+    player_query: Query<&Transform, With<Player>>,
+    level_query: Query<
+        (&Transform, &Handle<LdtkLevel>),
+        (Without<OrthographicProjection>, Without<Player>),
+    >,
+    level_selection: Res<LevelSelection>,
+    ldtk_levels: Res<Assets<LdtkLevel>>,
+) {
+    if let Ok(Transform {
+        translation: player_translation,
+        ..
+    }) = player_query.get_single()
+    {
+        let player_translation = *player_translation;
+
+        let (mut orthographic_projection, mut camera_transform) = camera_query.single_mut();
+
+        for (level_transform, level_handle) in level_query.iter() {
+            if let Some(ldtk_level) = ldtk_levels.get(level_handle) {
+                let level = &ldtk_level.level;
+                if level_selection.is_match(&0, level) {
+                    let level_ratio = level.px_wid as f32 / ldtk_level.level.px_hei as f32;
+
+                    orthographic_projection.scaling_mode = bevy::render::camera::ScalingMode::None;
+                    orthographic_projection.bottom = 0.;
+                    orthographic_projection.left = 0.;
+                    if level_ratio > ASPECT_RATIO {
+                        // level is wider than the screen
+                        orthographic_projection.top = (level.px_hei as f32 / 9.).round() * 9.;
+                        orthographic_projection.right = orthographic_projection.top * ASPECT_RATIO;
+                        camera_transform.translation.x = (player_translation.x
+                            - level_transform.translation.x
+                            - orthographic_projection.right / 2.)
+                            .clamp(0., level.px_wid as f32 - orthographic_projection.right);
+                        camera_transform.translation.y = 0.;
+                    } else {
+                        // level is taller than the screen
+                        orthographic_projection.right = (level.px_wid as f32 / 16.).round() * 16.;
+                        orthographic_projection.top = orthographic_projection.right / ASPECT_RATIO;
+                        camera_transform.translation.y = (player_translation.y
+                            - level_transform.translation.y
+                            - orthographic_projection.top / 2.)
+                            .clamp(0., level.px_hei as f32 - orthographic_projection.top);
+                        camera_transform.translation.x = 0.;
+                    }
+
+                    camera_transform.translation.x += level_transform.translation.x;
+                    camera_transform.translation.y += level_transform.translation.y;
+                }
+            }
         }
     }
 }
